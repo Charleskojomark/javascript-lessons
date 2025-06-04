9018ed45c66dfc9aa679e43e8a7b942c6309b75c
Charleskojomark <markcharleskojo@gmail.com>
2025-06-04 12:43:44 +0100

    add new javascript file

---
+++ new.js
@@ -0,0 +1,24 @@
+// Data types, Variables and operators
+// "=" is assignment operator
+// to declare a variable, we use let, var or const
+
+//Primitive datatypes. (Came with the language or it's natural to the lang)
+
+let firstName// String
+firstName = "Mark"
+firstName = "Charles"
+firstName = "Kojo"
+let age = 25 // Number
+let isMarried = false // boolean
+const religion = "Christianity" // constants are used to declare 
+// variables that do not change value
+// typeof
+//other datatypes
+// 1. null, undefined, 
+// console.log(typeof(age))
+console.log(firstName)
+
+// Naming Conventions
+// camelCase eg, firstName
+// snake_case eg. first_name
+// TitleCase or PascalCase eg. FirstName
