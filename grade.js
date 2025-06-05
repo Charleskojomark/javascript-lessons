b218732c5e3c1c23b52953048d61626c4b487fd4
Charleskojomark <markcharleskojo@gmail.com>
2025-06-05 12:19:15 +0100

    add javscript grade file

---
+++ grade.js
@@ -0,0 +1,35 @@
+// Conditionals and Operators
+// This is a simple grading system
+
+
+// if(condition){
+//     statement to be executed
+// }
+
+// if(condition){
+//     statement to be executed
+// }else{
+//         another statement to be executed
+// }
+
+// if(condition){
+//         statement to be executed
+// }else if(another condition){
+//     another statement to be executed
+// }else if(next condition){
+//     next statement to be executed
+// }else{
+//         do something
+// }
+
+let score = 40;
+
+if(score >= 90){
+    console.log("Grade: A");
+} else if(score >= 80){
+    console.log("Grade: B");
+}else if(score >= 70){
+    console.log("Grade: C");
+}else {
+    console.log("Grade: F");
+}
