bba5f8b5fe09ed7b537beae74cc3cbe730467609
Charleskojomark <markcharleskojo@gmail.com>
2025-06-05 12:53:45 +0100

    add age-checker file

---
+++ age-checker.js
@@ -0,0 +1,27 @@
+let age = 16
+let hasParentalConsent = false
+
+if(age >= 18){
+    console.log("You can watch the movie")
+}else if(age >= 13 && hasParentalConsent){
+    console.log("You can watch too")
+}else{
+    console.log("Access Denied")
+}
+
+
+
+
+
+
+// trueStatement && falseStatement ==> false
+// trueStatement || falseStatement ==> true
+// trueStatement && trueStatement ==> true
+// trueStatement || trueStatement ==> true
+// falseStatement && falseStatement ==> false
+// falseStatement || falseStatement ==> false
+// !falseStatement ==> true
+// !trueStatement ==> false
+
+
+
