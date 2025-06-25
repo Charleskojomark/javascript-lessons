a259ba476b30513c84606ab3eead832aeafae330
Charleskojomark <markcharleskojo@gmail.com>
2025-06-25 13:11:56 +0100

    functions

---
+++ FUNCTIONS/assignment.js
@@ -0,0 +1,15 @@
+// Multiply Two Numbers
+// Write a function that takes two numbers and returns their product.
+
+// Add Three Numbers
+// Write a function that adds three numbers and returns the result.
+
+// Convert Minutes to Seconds
+// Write a function that takes minutes as input and 
+// returns the equivalent in seconds.
+
+// Area of a Rectangle
+// Write a function that takes length and width and returns the area.
+
+// Double a Number
+// Write a function that takes a number and returns double its value.
